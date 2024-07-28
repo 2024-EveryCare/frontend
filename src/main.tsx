7c36dcd1d73fb805b1e8775261eb9549c6306ada
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-07-28 19:58:29 +0900

    약 검색 페이지 API 연동

--- src/main.tsx
+++ src/main.tsx
@@ -31,31 +31,28 @@ const queryClient = new QueryClient();
 //   return worker.start();
 // }
 // enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <RegisterContextProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<MainPg />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/signup2" element={<SiginupPage2 />} />
-              <Route path="/scan-or-direct" element={<ScanOrDirectPage />} />
-              <Route path="/direct-scan" element={<DirectScanPage />} />
-              <Route path="/scan-confirm" element={<ScanConfirmPage />} />
-              <Route path="/direct-register" element={<DirectRegisterPage />} />
-              <Route path="/pill-search" element={<PillSearchPage />} />
-              <Route path="/pill-register" element={<PillRegisterPage />} />
-              <Route path="/calendar" element={<CalendarPage />} />
-              <Route
-                path="/pill-info-search"
-                element={<PillInfoSearchPage />}
-              />
-            </Routes>
-          </Router>
-        </RegisterContextProvider>
-      </QueryClientProvider>
-    </React.StrictMode>,
-  );
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RegisterContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPg />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/signup2" element={<SiginupPage2 />} />
+            <Route path="/scan-or-direct" element={<ScanOrDirectPage />} />
+            <Route path="/direct-scan" element={<DirectScanPage />} />
+            <Route path="/scan-confirm" element={<ScanConfirmPage />} />
+            <Route path="/direct-register" element={<DirectRegisterPage />} />
+            <Route path="/pill-search" element={<PillSearchPage />} />
+            <Route path="/pill-register" element={<PillRegisterPage />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/pill-info-search" element={<PillInfoSearchPage />} />
+          </Routes>
+        </Router>
+      </RegisterContextProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
 // });
