78b67c77605554b532a21a09eb9a6c78d9a372ed
kingmusung <abuga991008@gmail.com>
2024-08-21 16:01:52 +0900

    ai 약사 채팅기능 퍼블리싱 완료(디테일 작업은 춯후 진행 예정)

--- src/components/AiChemist/Chat.tsx
+++ src/components/AiChemist/Chat.tsx
@@ -4,12 +4,21 @@ import ChemistImg from '../../assets/Chatbot.png';
 import UserImg from '../../assets/dad.png';
 
 const Chat: React.FC = () => {
-  const [messages, setMessages] = useState<{ user: string; text: string }[]>([]);
+  const [messages, setMessages] = useState<{ user: string; text: string }[]>(
+    [],
+  );
   const [input, setInput] = useState('');
 
   const handleSend = () => {
     if (input.trim() !== '') {
-      setMessages([...messages, { user: 'User', text: input }]);
+      setMessages([...messages, { user: 'User', text: input }]); //이전 메시지 저장 후, 키:값 형태로 객체 저장, 추후 메세지를 누가 보냈는지 구분하기 위함.
+      setInput(''); // 입력창 비우기
+    }
+  };
+
+  const handleSendAi = () => {
+    if (input.trim() !== '') {
+      setMessages([...messages, { user: 'Ai', text: input }]); //이전 메시지 저장 후, 키:값 형태로 객체 저장, 추후 메세지를 누가 보냈는지 구분하기 위함.
       setInput(''); // 입력창 비우기
     }
   };
@@ -21,28 +30,30 @@ const Chat: React.FC = () => {
         {messages.map((message, index) => (
           <div
             key={index}
-            className={`flex items-center space-x-2 ${
-              message.user === 'User' ? 'justify-end' : ''
-            }`}
+            className={`flex items-center space-x-2 
+                ${ //메시지 저장시 키,값 쌍의 객체형식으로 저장
+                  message.user === 'User' ? 'justify-end' : '' //유저가 맞다면 스타일에 justify-end를 둬서 오른쪽에 붙혀서 정렬, 아닐시(Ai약사) 기본 왼쪽정렬
+                }`}
           >
-            {message.user !== 'User' && (
+            {message.user !== 'User' && ( //유저가 아닐경우(Ai약사) 약사 이미지
               <img
                 className="w-[40px] h-[40px]"
                 src={ChemistImg}
                 alt="Chemist"
               />
             )}
+            {/* state에 저장되어진 message 표시되는곳 */}
             <div className="min-w-[100px] bg-white p-2 rounded-md shadow">
               {message.text}
             </div>
-            {message.user === 'User' && (
+            {message.user === 'User' && ( //유저가 맞을 경우 유저 이미지
               <img className="w-[40px] h-[40px]" src={UserImg} alt="User" />
             )}
           </div>
         ))}
       </div>
 
-      {/* 입력 필드 및 전송 버튼 */}
+      {/* 유저 입력필드 */}
       <div className="bottom-0 h-[3vh] w-[100%] absolute items-center space-x-2">
         <input
           className="flex-grow p-2 rounded-md shadow border border-gray-300 w-[80%]"
@@ -59,6 +70,28 @@ const Chat: React.FC = () => {
         <button
           className="p-2 bg-blue-500 text-white rounded-md shadow"
           onClick={handleSend}
+        >
+          전송
+        </button>
+      </div>
+
+      {/* ai 약사 입력필드, api연동 되면 삭제예정 */}
+      <div className="bottom-[10%] h-[3vh] w-[100%] absolute items-center space-x-2">
+        <input
+          className="flex-grow p-2 rounded-md shadow border border-gray-300 w-[80%]"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="메시지를 입력하세요..."
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handleSendAi();
+            }
+          }}
+        />
+        <button
+          className="p-2 bg-blue-500 text-white rounded-md shadow"
+          onClick={handleSendAi}
         >
           전송
         </button>
