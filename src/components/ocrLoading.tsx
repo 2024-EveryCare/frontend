352909ed4ea8257f5b46f3d731bf5ea097fa802c
kingmusung <abuga991008@gmail.com>
2024-08-29 22:09:03 +0900

    로딩창 디자인 변경

--- src/components/ocrLoading.tsx
+++ src/components/ocrLoading.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const ocrLoading: React.FC = () => {
+const OcrLoading: React.FC = () => {
   return (
-    <div className="absolute bg-gray-100 w-[100%] h-[100%] top-[0%]">
-      <div className="w-32 h-32 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin absolute bottom-[55%] left-[40%]"></div>
-      <p className="W-[100%] text-xl text-gray-700 absolute bottom-[45%] left-[15%]">
-        처방전 인식 중 입니다... (...최대 2분 소요)
+    <div className="absolute bg-gradient-to-r from-green-300 via-teal-300 to-sky-400 w-[100%] h-[100%] top-0 flex items-center justify-center">
+      <div className="relative">
+        <div className="w-24 h-24 border-4 border-white border-t-transparent border-solid rounded-full animate-spin"></div>
+        <div className="absolute top-0 left-0 w-24 h-24 border-4 border-white border-b-transparent border-solid rounded-full animate-spin-slower"></div>
+      </div>
+      <p className="text-2xl text-white absolute bottom-[40%] left-[50%] transform -translate-x-1/2 animate-pulse">
+        Loading...
       </p>
     </div>
   );
 };
 
-export default ocrLoading;
+export default OcrLoading;
